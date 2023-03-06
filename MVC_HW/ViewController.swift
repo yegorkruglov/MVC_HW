6b30e73a4de5166dc21496dd60024b10de0a46d5
Egor Kruglov <yegorkruglov@gmail.com>
2023-03-06 11:28:31 +0300

    Initial Commit

---
+++ MVC_HW/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVC_HW
+//
+//  Created by Egor Kruglov on 06.03.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
